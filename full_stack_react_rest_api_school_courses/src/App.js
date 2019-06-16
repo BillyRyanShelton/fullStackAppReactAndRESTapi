1f38f165add799b7e0c63396bccfd728ec414ee0
Billy Shelton <sheltonbilly@utexas.edu>
2019-06-16 00:43:27 -0500

    courses component now properly retrieves and renders the list of courses to the courses route.

--- full_stack_react_rest_api_school_courses/src/App.js
+++ full_stack_react_rest_api_school_courses/src/App.js
@@ -14,8 +14,8 @@ class Courses extends Component {
   }
   //Images are immediately loaded to the page with the props search topic
   componentDidMount() {
-    axios.get(`http://localhost:5000`).then((response) => {
-      const data = response.data;
+    axios.get(`http://localhost:5000/api/courses`).then((response) => {
+      const data = response.data.courses;
       console.log(data);
       this.setState({data});
     })
@@ -24,48 +24,62 @@ class Courses extends Component {
     }); 
   }
 
+
 render() {
-    return(
-<html lang="en">
 
-  <head>
-    <meta charset="utf-8"/>
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
-    <link rel="shortcut icon" href="/favicon.ico"/>
-    <link href="https://fonts.googleapis.com/css?family=Work+Sans:400,500" rel="stylesheet" type="text/css"/>
-    <link href="https://fonts.googleapis.com/css?family=Cousine" rel="stylesheet" type="text/css"/>
-    <link href="../styles/global.css" rel="stylesheet"/>
-    <title>Courses</title>
-  </head>
-
-  <body>
-    <div id="root">
-      <div>
-        <div class="header">
-          <div class="bounds">
-            <h1 class="header--logo">Courses</h1>
-            <nav><a class="signup" href="sign-up.html">Sign Up</a><a class="signin" href="sign-in.html">Sign In</a></nav>
-          </div>
-        </div>
-        <hr/>
-        <div class="bounds">
+      //If courses are returned they are stored
+    let data = this.state.data;
+    let courses;
+    if(data.length > 0) {
+      courses = data.map( (course) => 
           <div class="grid-33"><a class="course--module course--link" href="course-detail.html">
               <h4 class="course--label">Course</h4>
-              <h3 class="course--title">Build a Basic Bookcase</h3>
+              <h3 class="course--title">{course.title}</h3>
             </a>
           </div>
-          <div class="grid-33"><a class="course--module course--add--module" href="create-course.html">
-              <h3 class="course--add--title"><svg version="1.1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
-                  viewBox="0 0 13 13" class="add">
-                  <polygon points="7,6 7,0 6,0 6,6 0,6 0,7 6,7 6,13 7,13 7,7 13,7 13,6 "></polygon>
-                </svg>New Course</h3>
-            </a></div>
-        </div>
-      </div>
-    </div>
-  </body>
+      );
+    } //If no images are in the array then a Not Found message is displayed to the DOM 
+    else {
+      courses = <h1 className="not-found"> Not Found </h1>
+    }
 
-</html>
+    return(
+      <html lang="en">
+
+        <head>
+          <meta charset="utf-8"/>
+          <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
+          <link rel="shortcut icon" href="/favicon.ico"/>
+          <link href="https://fonts.googleapis.com/css?family=Work+Sans:400,500" rel="stylesheet" type="text/css"/>
+          <link href="https://fonts.googleapis.com/css?family=Cousine" rel="stylesheet" type="text/css"/>
+          <link href="../styles/global.css" rel="stylesheet"/>
+          <title>Courses</title>
+        </head>
+
+        <body>
+          <div id="root">
+            <div>
+              <div class="header">
+                <div class="bounds">
+                  <h1 class="header--logo">Courses</h1>
+                  <nav><a class="signup" href="sign-up.html">Sign Up</a><a class="signin" href="sign-in.html">Sign In</a></nav>
+                </div>
+              </div>
+              <hr/>
+              <div class="bounds">
+                {courses}
+                <div class="grid-33"><a class="course--module course--add--module" href="create-course.html">
+                    <h3 class="course--add--title"><svg version="1.1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
+                        viewBox="0 0 13 13" class="add">
+                        <polygon points="7,6 7,0 6,0 6,6 0,6 0,7 6,7 6,13 7,13 7,7 13,7 13,6 "></polygon>
+                      </svg>New Course</h3>
+                  </a></div>
+              </div>
+            </div>
+          </div>
+        </body>
+
+      </html>
     );
   }
 }
