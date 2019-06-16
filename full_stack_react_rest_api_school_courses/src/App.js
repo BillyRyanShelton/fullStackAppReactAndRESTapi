783b4e02b7344a0ad4cc6b4c2c38651ae7b71375
Billy Shelton <sheltonbilly@utexas.edu>
2019-06-15 23:11:03 -0500

    tested to make sure the rest api is accessible locally from the react application.

--- full_stack_react_rest_api_school_courses/src/App.js
+++ full_stack_react_rest_api_school_courses/src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import React from 'react';
+// import logo from './logo.svg';
+// import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react';
+import { BrowserRouter, Route, NavLink, Switch } from 'react-router-dom';
+import axios from 'axios';
+
+
+class App extends Component {
+
+  //State is created to handle the images from flickr's API and the topic to be searched
+  constructor() {
+    super();
+    this.state = {
+      data: ''
+    }
+  }
+  //Images are immediately loaded to the page with the props search topic
+  componentDidMount() {
+    axios.get(`http://localhost:5000`).then((response) => {
+      const data = response.data;
+      console.log(data);
+      this.setState({data});
+    })
+    .catch(error => {
+      console.log('Error fetching and parsing data.', error);
+    }); 
+  }
+
+
+
+render() {
+    return(
+        <div> 
+          { this.state.data.message}
+        </div>
+    );
+  }
 }
-
-export default App;
+export default App
