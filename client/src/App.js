b8699eed49bffef7eabc473994007c0f4407169f
Billy Shelton <sheltonbilly@utexas.edu>
2019-06-18 23:12:51 -0500

    fixed warning issue in the console.
    <html> tag cannont be inside <div> tag

--- client/src/App.js
+++ client/src/App.js
@@ -15,6 +15,12 @@ render() {
 }
 // export default App
 
+
+
+
+
+
+
 class UserSignUp extends Component {
 
 render() {
@@ -25,6 +31,17 @@ render() {
   }
 }
 // export default App
+
+
+
+
+
+
+
+
+
+
+
 
 class UserSignIn extends Component {
 
@@ -37,29 +54,109 @@ render() {
 }
 // export default App
 
+
+
+
+
+
+
+
+
+
+
 class CourseDetail extends Component {
 
+  constructor() {
+    super();
+    this.state = {
+      courseData: null
+    }
+  }
 
-  // //Images are immediately loaded to the page with the props search topic
-  // componentDidMount() {
-  //   axios.get(`http://localhost:5000/api/courses`).then((response) => {
-  //     const data = response.data.courses;
-  //     console.log(data);
-  //     this.setState({data});
-  //   })
-  //   .catch(error => {
-  //     console.log('Error fetching and parsing data.', error);
-  //   }); 
-  // }
+  //course detail is retrived from the database
+  componentDidMount() {
+    const id = this.props.match.params.id;
+    console.log(id);
+
+    axios.get(`http://localhost:5000/api/courses/${id}`).then((response) => {
+      const courseData = response.data.course;
+      console.log(courseData);
+      this.setState({courseData});
+    })
+    .catch(error => {
+      console.log('Error fetching and parsing data.', error);
+    }); 
+  }
 
 render() {
     return(
-      <div>
+      <div id="root">
+        <div>
+          <Header/>
+          <hr/>
+          <div>
+            <div className="actions--bar">
+              <div className="bounds">
+                <div className="grid-100"><span><a className="button" href="update-course.html">Update Course</a><a className="button" href="#">Delete Course</a></span><a
+                    className="button button-secondary" href="index.html">Return to List</a></div>
+              </div>
+            </div>
+            <div className="bounds course--detail">
+              <div className="grid-66">
+                <div className="course--header">
+                  <h4 className="course--label">Course</h4>
+                  <h3 className="course--title">Build a Basic Bookcase</h3>
+                  <p>By Joe Smith</p>
+                </div>
+                <div className="course--description">
+                  <p>High-end furniture projects are great to dream about. But unless you have a well-equipped shop and some serious woodworking experience to draw on, it can be difficult to turn the dream into a reality.</p>
+                  <p>Not every piece of furniture needs to be a museum showpiece, though. Often a simple design does the job just as well and the experience gained in completing it goes a long way toward making the next project even better.</p>
+                  <p>Our pine bookcase, for example, features simple construction and it's designed to be built with basic woodworking tools. Yet, the finished project is a worthy and useful addition to any room of the house. While it's meant to rest on the floor, you can convert the bookcase to a wall-mounted storage unit by leaving off the baseboard. You can secure the cabinet to the wall by screwing through the cabinet cleats into the wall studs.</p>
+                  <p>We made the case out of materials available at most building-supply dealers and lumberyards, including 1/2 x 3/4-in. parting strip, 1 x 2, 1 x 4 and 1 x 10 common pine and 1/4-in.-thick lauan plywood. Assembly is quick and easy with glue and nails, and when you're done with construction you have the option of a painted or clear finish.</p>
+                  <p>As for basic tools, you'll need a portable circular saw, hammer, block plane, combination square, tape measure, metal rule, two clamps, nail set and putty knife. Other supplies include glue, nails, sandpaper, wood filler and varnish or paint and shellac.</p>
+                  <p>The specifications that follow will produce a bookcase with overall dimensions of 10 3/4 in. deep x 34 in. wide x 48 in. tall. While the depth of the case is directly tied to the 1 x 10 stock, you can vary the height, width and shelf spacing to suit your needs. Keep in mind, though, that extending the width of the cabinet may require the addition of central shelf supports.</p>
+                </div>
+              </div>
+              <div className="grid-25 grid-right">
+                <div className="course--stats">
+                  <ul className="course--stats--list">
+                    <li className="course--stats--list--item">
+                      <h4>Estimated Time</h4>
+                      <h3>14 hours</h3>
+                    </li>
+                    <li className="course--stats--list--item">
+                      <h4>Materials Needed</h4>
+                      <ul>
+                        <li>1/2 x 3/4 inch parting strip</li>
+                        <li>1 x 2 common pine</li>
+                        <li>1 x 4 common pine</li>
+                        <li>1 x 10 common pine</li>
+                        <li>1/4 inch thick lauan plywood</li>
+                        <li>Finishing Nails</li>
+                        <li>Sandpaper</li>
+                        <li>Wood Glue</li>
+                        <li>Wood Filler</li>
+                        <li>Minwax Oil Based Polyurethane</li>
+                      </ul>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
 }
 // // export default App
+
+
+
+
+
+
+
 
 class UpdateCourse extends Component {
 
@@ -71,6 +168,14 @@ render() {
   }
 }
 // export default App
+
+
+
+
+
+
+
+
 
 class CreateCourse extends Component {
 
@@ -84,6 +189,18 @@ render() {
 }
 // export default App
 
+
+
+
+
+
+
+
+
+
+
+
+
 class Courses extends Component {
 
   constructor() {
@@ -94,9 +211,9 @@ class Courses extends Component {
   }
   //Images are immediately loaded to the page with the props search topic
   componentDidMount() {
-    axios.get(`http://localhost:5000/api/courses`).then((response) => {
+    axios.get('http://localhost:5000/api/courses').then((response) => {
       const data = response.data.courses;
-      console.log(data);
+      //console.log(data);
       this.setState({data});
     })
     .catch(error => {
@@ -112,9 +229,9 @@ render() {
     let courses;
     if(data.length > 0) {
       courses = data.map( (course) => 
-        <div class="grid-33"><a class="course--module course--link" href={'/courses/' + course.id}>
-            <h4 class="course--label">Course</h4>
-            <h3 class="course--title">{course.title}</h3>
+        <div className="grid-33"><a className="course--module course--link" href={'/courses/' + course.id}>
+            <h4 className="course--label">Course</h4>
+            <h3 className="course--title">{course.title}</h3>
           </a>
         </div>
       );
@@ -124,52 +241,19 @@ render() {
     }
 
     return(
-      <html lang="en">
-
-        <head>
-          <meta charset="utf-8"/>
-          <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
-          <link rel="shortcut icon" href="/favicon.ico"/>
-          <link href="https://fonts.googleapis.com/css?family=Work+Sans:400,500" rel="stylesheet" type="text/css"/>
-          <link href="https://fonts.googleapis.com/css?family=Cousine" rel="stylesheet" type="text/css"/>
-          <link href="App.css" rel="stylesheet"/>
-          <title>Courses</title>
-        </head>
-
-        <body>
-          <div id="root">
-            <div>
-              <Header/>
-              <hr/>
-              <div class="bounds">
-                {courses}
-                <div class="grid-33"><a class="course--module course--add--module" href='/courses/create'>
-                    <h3 class="course--add--title"><svg version="1.1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
-                        viewBox="0 0 13 13" class="add">
-                        <polygon points="7,6 7,0 6,0 6,6 0,6 0,7 6,7 6,13 7,13 7,7 13,7 13,6 "></polygon>
-                      </svg>New Course</h3>
-                  </a></div>
-              </div>
-            </div>
+      <div id="root">
+        <div>
+          <Header/>
+          <hr/>
+          <div className="bounds">
+            {courses}
+            <div className="grid-33"><a className="course--module course--add--module" href='/courses/create'>
+                <h3 className="course--add--title"><svg version="1.1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
+                    viewBox="0 0 13 13" className="add">
+                    <polygon points="7,6 7,0 6,0 6,6 0,6 0,7 6,7 6,13 7,13 7,7 13,7 13,6 "></polygon>
+                  </svg>New Course</h3>
+              </a></div>
           </div>
-        </body>
-
-      </html>
-    );
-  }
-}
-// export default App
-
-
-//need to add when a user is signed in for the header component
-class Header extends Component {
-
-render() {
-    return(
-      <div class="header">
-        <div class="bounds">
-          <h1 class="header--logo">Courses</h1>
-          <nav><a class="signup" href="sign-up.html">Sign Up</a><a class="signin" href="sign-in.html">Sign In</a></nav>
         </div>
       </div>
     );
@@ -181,18 +265,45 @@ render() {
 
 
 
+
+
+
+//need to add when a user is signed in for the header component
+class Header extends Component {
+
+render() {
+    return(
+      <div className="header">
+        <div className="bounds">
+          <h1 className="header--logo">Courses</h1>
+          <nav><a className="signup" href="sign-up.html">Sign Up</a><a className="signin" href="sign-in.html">Sign In</a></nav>
+        </div>
+      </div>
+    );
+  }
+}
+// export default App
+
+
+
+
+
+
+
+
+
 class App extends Component {
   render() {
     return(
       <BrowserRouter>
         <Switch>
-          <Route exact path='/' render={() => <Courses/>}/>
-          <Route exact path='/courses/create' render={() => <CreateCourse/>}/>
-          <Route exact path='/courses/:id/update' render={() => <UpdateCourse/>}/>
-          <Route exact path='/courses/:id' render={() => <CourseDetail/>}/>
-          <Route exact path='/signin' render={() => <UserSignIn/>}/>
-          <Route exact path='/signup' render={() => <UserSignUp/>}/>
-          <Route exact path='/signout' render={() => <UserSignOut/>}/>
+          <Route exact path='/' component={Courses}/>
+          <Route exact path='/courses/create' component={CreateCourse}/>
+          <Route exact path='/courses/:id/update' component={UpdateCourse}/>
+          <Route exact path='/courses/:id' component = {CourseDetail}/>
+          <Route exact path='/signin' component={UserSignIn}/>
+          <Route exact path='/signup' component={UserSignUp}/>
+          <Route exact path='/signout' component={UserSignOut}/>
         </Switch>
       </BrowserRouter>
     );
